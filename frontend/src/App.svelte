d6fe636445a31ed8f9ca405e43dead16ac38add8
Khaled_Dawoud <kdawoud56@gmail.com>
2023-09-26 20:56:25 +0300

    Initial Commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<style>
+  @import 'tailwindcss/base.css';
+  @import 'tailwindcss/components.css';
+  @import 'tailwindcss/utilities.css';
+</style>
+
+<script lang="ts">
+  import LoginForm from "./lib/LoginForm.svelte";
+  import LoginBackground from "./assets/LoginBackground.jpg";
+
+
+</script>
+
+<body class="bg-cover bg-no-repeat bg-center" style="background-image: url('{LoginBackground}');">
+  <div class="flex min-h-screen justify-center items-center align-middle">
+    <LoginForm />
+  </div>
+</body>
+
+
