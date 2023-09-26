6c422624f782fb6f5746a293cdb02ec942a0e864
KhDawoud <kdawoud56@gmail.com>
2023-09-26 23:04:23 +0300

    Fixed Responsiveness On Icons

--- frontend/src/lib/LoginForm.svelte
+++ frontend/src/lib/LoginForm.svelte
@@ -3,7 +3,7 @@
     let password: string;
 </script>
 
-<div class="bg-white rounded-2xl w-full lg:w-1/3 md:w-2/3 h-[30rem] m-7 mt-8 p-12 shadow-lg">
+<div class="bg-white rounded-2xl w-full lg:w-1/3 md:w-2/3 h-[30rem] m-7 mt-8 p-12 shadow-2xl">
     <h1 class="text-center font-semibold text-4xl"> Login </h1>
     <form class="mt-10">
         <div class="relative">
@@ -30,28 +30,28 @@
         <div class="mt-6 text-center">
             <hr>
             <h2 class="mt-2">Or Sign In With</h2>
-            <div class="flex justify-between mt-4 md:px-36">
-                <button>
-                    <svg id="google" xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 488 512">
-                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <style>#google{fill:#ff0000}</style>
-                    <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
-                    </svg>
-                </button>
-                <button>
-                    <svg id="facebook" xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 512 512">
+            <div class="flex justify-center mt-4">
+                    <button class="px-4">
+                        <svg id="google" xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 488 512">
                         <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                            <style>#facebook{fill:#0080ff}</style>
-                            <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/>
-                    </svg>
-                </button>
-                <button>
-                    <svg id="twitter" xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 448 512">
-                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                        <style>#twitter{fill:#000000}</style>
-                        <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/>
-                    </svg>
-                </button>
+                        <style>#google{fill:#ff0000}</style>
+                        <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
+                        </svg>
+                    </button>
+                    <button class="px-4">
+                        <svg id="facebook" xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 512 512">
+                            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <style>#facebook{fill:#0080ff}</style>
+                                <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/>
+                        </svg>
+                    </button>
+                    <button class="px-4">
+                        <svg id="twitter" xmlns="http://www.w3.org/2000/svg" height="2.5em" viewBox="0 0 448 512">
+                            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <style>#twitter{fill:#000000}</style>
+                            <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/>
+                        </svg>
+                    </button>
             </div>
         </div>
     </form>
