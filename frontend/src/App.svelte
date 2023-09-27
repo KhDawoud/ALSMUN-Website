f8bf2f529e577c678ac65dd1e10bf09df2752212
KhDawoud <kdawoud56@gmail.com>
2023-09-27 09:06:45 +0300

    Implemented Routing to home page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,20 +1,14 @@
-<style>
-  @import 'tailwindcss/base.css';
-  @import 'tailwindcss/components.css';
-  @import 'tailwindcss/utilities.css';
-</style>
-
 <script lang="ts">
+  import { Router, Link, Route } from 'svelte-routing';
   import LoginForm from "./lib/LoginForm.svelte";
-  import LoginBackground from "./assets/LoginBackground.jpg";
-
-
+  import Home from './lib/Home.svelte';
 </script>
 
-<body class="bg-cover bg-no-repeat bg-center" style="background-image: url('{LoginBackground}');">
-  <div class="flex min-h-screen justify-center items-center align-middle">
-    <LoginForm />
-  </div>
-</body>
+
+
+<Router>
+  <Route path="/" component={LoginForm} />
+  <Route path='/home' component={Home} />
+</Router>
 
 
