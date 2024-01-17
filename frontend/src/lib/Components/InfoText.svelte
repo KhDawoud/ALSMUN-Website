963288209f5737ef28761176b7b02bd362b2c401
KhDawoud <kdawoud56@gmail.com>
2024-01-17 23:22:57 +0200

    Designed Committee pages

---
+++ frontend/src/lib/Components/InfoText.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let image: string;
+    export let title: string;
+    export let paragraphA: string;
+    export let paragraphB: string;
+</script>
+
+
+
+<div class="border rounded-lg shadow p-7 mx-10">
+
+  <div class="flex w-full">
+
+    <div class="w-1/2">
+        <header class="ps-4">
+            <h2 class="font-bold text-2xl mb-2"> {title} </h2>
+            <hr class=" border-b mb-5">
+        </header>
+        <p class="p-4">{paragraphA}</p>
+    </div>
+
+    <div class="w-1/2">  
+      <div class="ms-4">
+        <img src={image} alt="Topic" class="w-full h-auto">
+      </div>
+    </div>
+  </div>
+
+  <div class="w-full">
+    <div class="p-4">
+      <p>{paragraphB}</p>
+    </div>
+  </div>
+
+</div>
+  
--- frontend/src/lib/Pages/Committee Pages/GA.svelte
+++ frontend/src/lib/Pages/Committee Pages/GA.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
   import NavBar from "../../Components/NavBar.svelte";
-
+  import InfoText from "../../Components/InfoText.svelte";
+  import TopicA from '../../../assets/Committees/GA/Topic 1.jpg';
+  import TopicB from '../../../assets/Committees/GA/Topic 2.jpg'
 
 </script>
 
 <NavBar />
+
+<h1 class="text-center font-bold underline text-6xl my-10"> General Assembly </h1>
+<div class="grid grid-flow-row mx-auto space-y-8">
+  <InfoText 
+    image={TopicA}
+    title="Topic 1: The Question of Whether Social and Security Concerns Call for Niqab Baba and Regulation"
+    paragraphB="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim diam vulputate ut pharetra sit amet aliquam. Aliquam ut porttitor leo a diam sollicitudin tempor id. Faucibus turpis in eu mi. Auctor eu augue ut lectus arcu bibendum. Mattis pellentesque id nibh tortor id aliquet. Suscipit tellus mauris a diam maecenas. Arcu risus quis varius quam quisque id. Maecenas volutpat blandit aliquam etiam erat velit. Luctus accumsan tortor posuere ac ut consequat semper viverra.
+    Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Nulla malesuada pellentesque elit eget gravida. Bibendum neque egestas congue quisque. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Vulputate odio ut enim blandit. Mus mauris vitae ultricies leo integer malesuada nunc. Augue interdum velit euismod in pellentesque massa placerat duis. Netus et malesuada fames ac turpis. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Integer vitae justo eget magna fermentum iaculis. Egestas diam in arcu cursus euismod quis viverra nibh. Placerat orci nulla pellentesque dignissim enim. Diam sollicitudin tempor id eu nisl nunc. Feugiat pretium nibh ipsum consequat nisl vel. Sit amet nisl purus in mollis nunc sed id. A scelerisque purus semper eget. Vitae auctor eu augue ut lectus arcu bibendum at.
+    Sit amet tellus cras adipiscing enim eu turpis egestas."
+    paragraphA="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. elit pellentesque habitant. Nulla malesuada pellentesque elit eget gravida. Bibendum neque egestas congue quisque. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Vulputate odio ut enim blandit. Mus mauris vitae ultricies leo integer malesuada nunc. Augue interdum velit euismod in pellentesque massa placerat duis. Netus et malesuada fames ac turpis. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Integer"
+  />
+
+  <InfoText 
+    image={TopicB} 
+    title="Topic 2: Developing Internal Frameworks to Combat Animal Cruelty and Unethical Practices in Slaughterhouses and Fashion Industries"
+    paragraphB="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim diam vulputate ut pharetra sit amet aliquam. Aliquam ut porttitor leo a diam sollicitudin tempor id. Faucibus turpis in eu mi. Auctor eu augue ut lectus arcu bibendum. Mattis pellentesque id nibh tortor id aliquet. Suscipit tellus mauris a diam maecenas. Arcu risus quis varius quam quisque id. Maecenas volutpat blandit aliquam etiam erat velit. Luctus accumsan tortor posuere ac ut consequat semper viverra.
+    Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Nulla malesuada pellentesque elit eget gravida. Bibendum neque egestas congue quisque. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Vulputate odio ut enim blandit. Mus mauris vitae ultricies leo integer malesuada nunc. Augue interdum velit euismod in pellentesque massa placerat duis. Netus et malesuada fames ac turpis. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Integer vitae justo eget magna fermentum iaculis. Egestas diam in arcu cursus euismod quis viverra nibh. Placerat orci nulla pellentesque dignissim enim. Diam sollicitudin tempor id eu nisl nunc. Feugiat pretium nibh ipsum consequat nisl vel. Sit amet nisl purus in mollis nunc sed id. A scelerisque purus semper eget. Vitae auctor eu augue ut lectus arcu bibendum at.
+    Sit amet tellus cras adipiscing enim eu turpis egestas."
+    paragraphA="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. elit pellentesque habitant. Nulla malesuada pellentesque elit eget gravida. Bibendum neque egestas congue quisque. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Vulputate odio ut enim blandit. Mus mauris vitae ultricies leo integer malesuada nunc. Augue interdum velit euismod in pellentesque massa placerat duis. Netus et malesuada fames ac turpis. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Integer"
+    />
+</div>
+
