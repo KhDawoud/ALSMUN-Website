0fac46c7b674d7873879e4b7b5feb4495ddc5d4a
MelonLord8 <melkhedr8@gmail.com>
2024-01-27 19:29:40 +0200

    Added Carousel to Home Page

--- frontend/src/lib/Pages/Home.svelte
+++ frontend/src/lib/Pages/Home.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
   import NavBar from "../Components/NavBar.svelte";
-
-
+  import Carousel from "../Components/Carousel.svelte";
+  import Schedule1 from "../../assets/Schedule1.jpg"
+  import Schedule2 from "../../assets/Schedule2.jpg"
+  import Schedule3 from "../../assets/Schedule3.jpg"
+  let images = [Schedule1, Schedule2, Schedule3]
 </script>
 
 <NavBar />
+<div>
+  <Carousel perPage = "1" duration = 600>
+    {#each images as image}
+      <div class = "flex items-center justify-center h-fit">
+        <img src = {image}/>
+      </div>
+    {/each}
+    <span slot="left-control" class= "text-9xl text-slate-700">&laquo;</span>
+    <span slot="right-control" class ="text-9xl text-slate-700">&raquo;</span>
+  </Carousel>
+</div>
